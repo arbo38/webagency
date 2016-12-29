ae43ec38417e46c59aa09b6614da5243a579d3b0
arbo38 <beau.antony@gmail.com>
2016-12-29 11:31:39 +0100

    Footer fonctionnel grand ecran - Responsive reste a faire

--- js/webAgency.js
+++ js/webAgency.js
@@ -2,19 +2,19 @@
 function webAgency(){
 
     console.log("Script Initialisé");
-    
+
     function myMap(){
         var myCenter = new google.maps.LatLng(52.365948, 4.871017);
         var mapCanvas = document.getElementById("mapContainer");
         var mapOptions = {
             center: myCenter,
             zoom: 14,
-            disableDefaultUI: true
+            //disableDefaultUI: true
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
         var marker = new google.maps.Marker({position:myCenter});
         marker.setMap(map);
     }
-    
+
     myMap();
 }
